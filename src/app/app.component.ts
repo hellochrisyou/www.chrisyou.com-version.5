f0c710c22bbfde274108f11f7b9e272fc3ab8a7a
Chris You <28457425+hellochrisyou@users.noreply.github.com>
2020-03-13 13:52:48 -0400

    Finalized minor adjustments for mat group header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
+
 })
 export class AppComponent implements OnInit {
   title = 'Chris You\'s Website';
-
+  clear = 'transparent';
   ngOnInit(): void {
     $('.menu-collapsed').click(function () {
       $(this).toggleClass('menu-expanded');
