84ece90789e8839252921a8849facba48de25aa9
Chris You <28457425+hellochrisyou@users.noreply.github.com>
2020-03-23 06:21:53 -0400

    CLeanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,23 @@
-import { SkillsComponent } from './skills/skills.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { TestimonialComponent } from './testimonial/testimonial.component';
-import { SkillComponent } from './skill/skill.component';
-import { ProjectComponent } from './project/project.component';
+import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { RecordingComponent } from './recording/recording.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AboutComponent } from './about/about.component';
 import { AppRoutingModule } from './app-routing.module';
-import { PrevVersionComponent } from './prev-version/prev-version.component';
-import { BackgroundComponent } from './background/background.component';
+import { AppComponent } from './app.component';
 import { BackgroundMobileComponent } from './background-mobile/background-mobile.component';
+import { BackgroundComponent } from './background/background.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { MaterialModule } from './material.module';
+import { PrevVersionComponent } from './prev-version/prev-version.component';
+import { ProjectComponent } from './project/project.component';
+import { RecordingComponent } from './recording/recording.component';
+import { SkillsComponent } from './skills/skills.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
 
 @NgModule({
   declarations: [
